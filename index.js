6ac4dde7a04c2e675e923402e34059fa01995fe8
HuihanLi <huihan.li@addomain.org>
2017-07-18 14:50:14 -0400

    added the first File

---
+++ index.js
@@ -0,0 +1,30 @@
+//first, create the viz
+title_sheet = {
+        "WEEKLY TV IMPRESSIONS": "WEEKLY TV/HULU IMPRESSIONS",
+        "WEEKLY PRINT CIRCULATION": "MONTHLY PRINT CIRCULATION",
+        "WEEKLY RADIO IMPRESSIONS": "WEEKLY RADIO IMPRESSIONS",
+        "FACEBOOK IMPRESSIONS": "WEEKLY FACEBOOK IMPRESSIONS",
+        "FACEBOOK ENGAGEMENT": "WEEKLY FACEBOOK ENGAGEMENT",
+        "PREROLL VIDEO IMPRESSIONS": "WEEKLY PREROLL VIDEO IMPRESSIONS",
+        "PREROLL VIDEO CTR": "WEEKLY PREROLL VIDEO CTR",
+        "NATIVE IMPRESSIONS": "WEEKLY NATIVE IMPRESSIONS",
+        "NATIVE CTR": "WEEKLY NATIVE CTR",
+        "FACEBOOK SENTIMENT": "WEEKLY FACEBOOK SENTIMENT",
+        "SITE VISIT": "WEEKLY SITE VISIT"
+     };
+
+col_alias = {
+  "[excel-direct.1dbzcdg1ts2uwx16oonsx1bx6wsb].[usr:Calculation_284008305897496576:qk]": "Hulu",
+  "[excel-direct.1dbzcdg1ts2uwx16oonsx1bx6wsb].[sum:W25-54 Impressions:qk]": "TV",
+  "[textscan.0odnxxs0zp1l2n18cnved0wpyswx].[sum:page_impressions_organic:qk]": "Page_Impressions_Organic",
+  "[textscan.0odnxxs0zp1l2n18cnved0wpyswx].[sum:page_impressions_paid:qk]": "Page_Impressions_Paid",
+  "[federated.0omo11h0k9xghi10nnzcl02angis].[usr:Calculation_517632548688437249:qk]": "Weekly Negative",
+  "[federated.0omo11h0k9xghi10nnzcl02angis].[usr:Calculation_635289030116114432:qk]": "Weekly Neutral",
+  "[federated.0omo11h0k9xghi10nnzcl02angis].[usr:Calculation_517632548688109568:qk]": "Weekly Positive",
+  "[textscan.1lwe0ud158tikj17tyowb0jmmhem].[sum:pageviews:qk]": "Pageviews",
+  "[textscan.1lwe0ud158tikj17tyowb0jmmhem].[sum:uniquePageviews:qk]": "uniquePageviews",
+  "[textscan.0odnxxs0zp1l2n18cnved0wpyswx].[sum:page_positive_feedback_link:qk]": "Share",
+  "[textscan.0odnxxs0zp1l2n18cnved0wpyswx].[sum:page_positive_feedback_comment:qk]": "Comment",
+  "[textscan.0odnxxs0zp1l2n18cnved0wpyswx].[sum:page_positive_feedback_like:qk]": "Like"
+
+};
